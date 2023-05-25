071658350a5b341571e0d83b51dbf11e4fabcde9
Huynh Nguyen <huynh.nguyen@opskins.com>
2023-05-25 22:18:14 +0700

    add env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
@@ -39,6 +40,7 @@ wss.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
+
+server.listen(process.env.PORT ?? 3000, () => {
+  console.log(`Server started on port ${process.env.PORT ?? 3000}`);
 });
